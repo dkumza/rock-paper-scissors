3f38fea0f38f4c029fd6398fa255abbeeb3f90b4
Darius Kumza <dariuskumza@gmail.com>
2023-01-08 19:54:08 +0200

    not final game

--- script.js
+++ script.js
@@ -9,12 +9,46 @@ const p_Score = document.querySelector('#player-score');
 const c_Score = document.querySelector('#pc-score');
 const p_chose = document.querySelector('#player-choose');
 const pc_chose = document.querySelector('#pc-choose');
-// const r_content = document.querySelector('#win');
 const toogle_cont = document.querySelector('#btn-play');
 const rr_game = document.querySelector('#restart');
-const para1 = document.createElement('p1');
 
-// const rr_btn = document.createElement('button');
+function draw_game() {
+  const n_list = document.createElement('li');
+  const n_list_txt = document.createTextNode('Draw');
+  n_list.appendChild(n_list_txt);
+  const list = document.getElementById('history-list');
+  list.insertBefore(n_list, list.children[0]);
+  document.querySelector('#game-cont').style.cssText = `
+  background-color: rgba(235, 203, 139, 0.3);`;
+}
+
+function player_round(playerChoice, computerChoice) {
+  const n_list = document.createElement('li');
+  const n_list_txt = document.createTextNode(
+    `Yours: ${playerChoice} wins vs Computer: ${computerChoice}`
+  );
+  n_list.appendChild(n_list_txt);
+  const list = document.getElementById('history-list');
+  list.insertBefore(n_list, list.children[0]);
+  playerScore += 1;
+  p_Score.innerHTML = playerScore;
+  document.querySelector('#game-cont').style.cssText = `
+  background-color: rgba(163, 190, 140, 0.3);`;
+}
+
+function pc_round(playerChoice, computerChoice) {
+  const n_list = document.createElement('li');
+  const n_list_txt = document.createTextNode(
+    `Computer with: ${computerChoice} wins vs yours: ${playerChoice}`
+  );
+  n_list.appendChild(n_list_txt);
+  const list = document.getElementById('history-list');
+  list.insertBefore(n_list, list.children[0]);
+  computerScore += 1;
+  c_Score.innerHTML = computerScore;
+  document.querySelector('#game-cont').style.cssText = `
+  background-color: rgba(208, 135, 112, 0.3);`;
+}
 
 // RANDOM VALUE OF "COMPUTER" TO START GAME
 function getComputerChoice() {
@@ -36,30 +70,43 @@ function playRound(playerChoice, computerChoice) {
   round++;
   r_Score.innerHTML = round;
   if (playerChoice == computerChoice) {
-    alert('DRAW: Computer tooked same "tool" as you did');
+    draw_game();
   } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
-    alert(`Yours: ${playerChoice} wins vs Computer: ${computerChoice}`);
-    playerScore += 1;
-    p_Score.innerHTML = playerScore;
+    // alert(`Yours: ${playerChoice} wins vs Computer: ${computerChoice}`);
+    player_round(playerChoice, computerChoice);
   } else if (playerChoice == 'paper' && computerChoice == 'rock') {
-    alert(`Yours: ${playerChoice} wins vs Computer: ${computerChoice}`);
-    playerScore += 1;
-    p_Score.innerHTML = playerScore;
+    // alert(`Yours: ${playerChoice} wins vs Computer: ${computerChoice}`);
+    player_round(playerChoice, computerChoice);
   } else {
-    alert(`Computer with: ${computerChoice} wins vs yours: ${playerChoice}`);
-    computerScore += 1;
-    c_Score.innerHTML = computerScore;
+    // alert(`Computer with: ${computerChoice} wins vs yours: ${playerChoice}`);
+    pc_round(playerChoice, computerChoice);
+  }
+
+  function player_wins() {
+    const n_list = document.createElement('li');
+    const n_list_txt = document.createTextNode(`YOU WIN!`);
+    n_list.appendChild(n_list_txt);
+    const list = document.getElementById('history-list');
+    list.insertBefore(n_list, list.children[0]);
+  }
+
+  function pc_wins() {
+    const n_list = document.createElement('li');
+    const n_list_txt = document.createTextNode(`COMPUTER WINS...`);
+    n_list.appendChild(n_list_txt);
+    const list = document.getElementById('history-list');
+    list.insertBefore(n_list, list.children[0]);
   }
 
   if (playerScore == 5) {
     toogle_cont.style.display = 'none';
     // creates <p> with message with greatings
-    para1.textContent = 'YOU WIN!';
+    player_wins();
     // r_content.appendChild(para1);
   } else if (computerScore == 5) {
     toogle_cont.style.display = 'none';
     // creates <p> with message with greatings
-    para1.textContent = 'COMPUTER WINS!';
+    pc_wins();
     // r_content.appendChild(para1);
   }
 }
